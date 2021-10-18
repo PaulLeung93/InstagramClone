6cb1d620acd3fc7db1964185c1458b441b99cc12
PaulLeung93 <78509075+PaulLeung93@users.noreply.github.com>
2021-10-17 23:44:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("nU2Zxaxq9rPIYUj0ehTMV9n1PxquZiWva6H0YNwX")
+                .clientKey("8lTjBhXcqTXGDeGzTWmDOOtRyNviSNyUFC9qIuva")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
